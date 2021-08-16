c6a594fbed23f3878478289157865566741d1760
pavan <badepallipavan@gmail.com>
2021-08-16 12:42:40 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+# Create your views here.
+
+def pavan1(request):
+    return render(request,'pavan1.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pavan2(request):
+    return render(request,'pavan2.html')
